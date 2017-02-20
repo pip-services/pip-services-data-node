e87981b177b0b157819d0dd6eaf1467e036081d7
Sergey Seroukhov <seroukhov@gmail.com>
2017-02-19 17:32:21 -0800

    'Fixed test configuration'

--- component.conf.ps1
+++ component.conf.ps1
@@ -2,6 +2,11 @@ $VersionControl = 'git'
 $Package = 'npm'
 $Build = 'typescript'
 $Document = 'none'
+
 $Test = 'mocha'
+$TestInclude = './obj/test'
+$TestTimeout = 10000
+$TestStyle = 'tdd'
+
 $Deploy = 'none'
 $Run = 'none'
